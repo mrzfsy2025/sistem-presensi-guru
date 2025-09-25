26536de1e3b185fb71dc5aa7c94094fe4e77aee4
mrzfsy~05 <mrzsy68056@yahoo.com>
2025-09-26 00:19:42 +0700

    Fitur: Mengintegrasikan Vite untuk frontend

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 8080;
-
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 // =============================================================
 // MENYAJIKAN FILE FRONTEND
---
+++ vite.config.js
@@ -0,0 +1,28 @@
+// File: /ABSENSI/vite.config.js
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    // Tentukan folder output
+    outDir: '../public',
+    // Kosongkan folder output sebelum build
+    emptyOutDir: true,
+    rollupOptions: {
+      // Tentukan file HTML mana saja yang menjadi halaman utama
+      input: {
+        main: resolve(__dirname, 'src/dashboard_utama.html'),
+        login: resolve(__dirname, 'src/login.html'),
+        login_guru: resolve(__dirname, 'src/login-guru.html'),
+        app_guru: resolve(__dirname, 'src/app-guru.html'),
+        manajemen_guru: resolve(__dirname, 'src/manajemen-guru.html'),
+        manajemen_izin: resolve(__dirname, 'src/manajemen-izin.html'),
+        laporan: resolve(__dirname, 'src/laporan.html'),
+        koreksi_presensi: resolve(__dirname, 'src/koreksi-presensi.html'),
+        pengaturan: resolve(__dirname, 'src/pengaturan.html'),
+      },
+    },
+  },
+  // Tentukan folder akar (root) dari source code frontend
+  root: 'src',
+});
