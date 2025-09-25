5574d25bcd080f4e2bb208a340c8f1eca5b76ebf
mrzfsy~05 <mrzsy68056@yahoo.com>
2025-09-26 00:50:04 +0700

    Fix: Menambahkan host 0.0.0.0 untuk deployment

--- server.js
+++ server.js
@@ -3,11 +3,15 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 8080;
+const PORT = process.env.PORT || 8080; 
+
 // Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
+app.listen(PORT, '0.0.0.0', () => { // <-- TAMBAHKAN '0.0.0.0'
+    console.log(`Server berjalan di port ${PORT}`);
+});
 
 // =============================================================
 // MENYAJIKAN FILE FRONTEND
