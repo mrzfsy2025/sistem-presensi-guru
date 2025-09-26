3ec6bff01f889f626993002715bdbbce3a2e030e
mrzfsy~05 <mrzsy68056@yahoo.com>
2025-09-26 13:02:43 +0700

    Fix: Menyesuaikan struktur untuk routing Vercel

--- api/server.js
+++ api/server.js
@@ -9,7 +9,6 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
 
 // =============================================================
 // MENGHUBUNGKAN SEMUA RUTE API
