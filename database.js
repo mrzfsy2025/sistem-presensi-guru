9c1c8cddf176992d4cca7c8a0534ac807be66d38
mrzfsy~05 <mrzsy68056@yahoo.com>
2025-09-28 17:37:34 +0700

    Percobaan Deployment via Railway-3

--- database.js
+++ database.js
@@ -1,16 +1,5 @@
-// File: /api/database.js (VERSI LANGSUNG)
+// File: /api/database.js 
 
 const mysql = require('mysql2/promise');
-
-// Konfigurasi koneksi langsung ke database XAMPP Anda
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 't/068fA@6fqoFG53', 
-    database: 'absensi_db', 
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
-
+const pool = mysql.createPool(process.env.DATABASE_URL);
 module.exports = pool;
