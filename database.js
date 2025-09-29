df169a200793bc95a1bcf02519633702e27b50ca
mrzfsy~05 <mrzsy68056@yahoo.com>
2025-09-29 13:26:38 +0700

    Tes database dan Reset password Railway-01

--- database.js
+++ database.js
@@ -2,4 +2,12 @@
 
 const mysql = require('mysql2/promise');
 const pool = mysql.createPool(process.env.DATABASE_URL);
+pool.getConnection()
+    .then(connection => {
+        console.log('Berhasil terhubung ke database MySQL!');
+        connection.release(); // Lepaskan koneksi setelah selesai
+    })
+    .catch(err => {
+        console.error('Gagal terhubung ke database:', err.stack);
+    });
 module.exports = pool;
