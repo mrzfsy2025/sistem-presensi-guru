a56a4ca035790cb3afc9d4f9f1222fa76d938b6c
mrzfsy~05 <mrzsy68056@yahoo.com>
2025-09-29 11:13:13 +0700

    Percobaan Reset Password Railway-05d

--- routes/auth.js
+++ routes/auth.js
@@ -100,7 +100,7 @@ router.post('/forgot-password', async (req, res) => {
             service: 'gmail',
             auth: {
                 user: process.env.EMAIL_USER, 
-                pass: process.env.EMAIL_PASS,  
+                pass: process.env.EMAIL_PASS  
             }
         });        
 
